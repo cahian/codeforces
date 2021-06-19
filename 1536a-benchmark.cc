950a8c452aa845f77ad1b36b23d3569d85140c99
Cahian Freire <cahianfreire@gmail.com>
2021-06-19 19:17:45 -0300

    Fix benchmark

--- 1536a-benchmark.cc
+++ 1536a-benchmark.cc
@@ -1,38 +1,55 @@
 #include <benchmark/benchmark.h>
 
+#include <vector>
+
 #include "1536a-possible-solutions.c"
 
-static AvlTree *GlobalAvlTree = NULL;
+static std::vector<AvlTree *> custom_args;
+static int custom_args_size = 128;
+static int custom_args_step = 128;
 
-static void CustomArguments(benchmark::internal::Benchmark *b) {
-  int i;
-  for (i = 1; i <= (1 << 29); i <<= 1) {
-    AvlTree_insert(&GlobalAvlTree, i);
-    b->Args({(long int)GlobalAvlTree});
+static void InitializeCustomArguments() {
+  custom_args.reserve(custom_args_size);
+  for (int i = 0; i < custom_args_size; ++i) {
+    AvlTree *avl_tree = NULL;
+    for (int j = 0; j < (custom_args_step * i); j += custom_args_step) {
+      AvlTree_insert(&avl_tree, j);
+    }
+    custom_args.push_back(avl_tree);
   }
-  for (i = 1; i <= (1 << 29); i <<= 1) {
-    AvlTree_insert(&GlobalAvlTree, -i);
-    b->Args({(long int)GlobalAvlTree});
-  }
+}
 
-  AvlTree_destroy(GlobalAvlTree);
-  GlobalAvlTree = NULL;
+static void DestroyCustomArguments() {
+  for (AvlTree *avl_tree : custom_args) {
+    AvlTree_destroy(avl_tree);
+  }
 }
 
 static void BM_AvlTree_in_order(benchmark::State &state) {
   for (auto _ : state) {
-    AvlTree_in_order((AvlTree *)state.range(0));
+    AvlTree_in_order(custom_args[state.range(0)]);
   }
 }
 
-BENCHMARK(BM_AvlTree_in_order)->Apply(CustomArguments);
+BENCHMARK(BM_AvlTree_in_order)->DenseRange(0, custom_args_size - 1, 1);
 
 static void BM_AvlTree_iterative_in_order(benchmark::State &state) {
   for (auto _ : state) {
-    AvlTree_iterative_in_order((AvlTree *)state.range(0));
+    AvlTree_iterative_in_order(custom_args[state.range(0)]);
   }
 }
 
-BENCHMARK(BM_AvlTree_iterative_in_order)->Apply(CustomArguments);
+BENCHMARK(BM_AvlTree_iterative_in_order)
+    ->DenseRange(0, custom_args_size - 1, 1);
 
-BENCHMARK_MAIN();
+int main(int argc, char **argv) {
+  InitializeCustomArguments();
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
+    return 1;
+  }
+  ::benchmark::RunSpecifiedBenchmarks();
+  ::benchmark::Shutdown();
+  DestroyCustomArguments();
+  return 0;
+}
